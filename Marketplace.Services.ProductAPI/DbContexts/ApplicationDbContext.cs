6ef19117e18410e48e57a47e423cdc926f871447
flashm123 <n_plk7a@mail.ru>
2022-09-20 22:42:19 +0300

    Add Product microservice project

---
+++ Marketplace.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Marketplace.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
